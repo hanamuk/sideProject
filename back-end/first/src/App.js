94910cb9cd3cba4feaa3379341cee595621da6ce
hanamuk <46813878+hanamuk@users.noreply.github.com>
2021-03-26 22:08:25 +0900

    1

---
+++ back-end/first/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+//로컬 3000포트에서 동작중
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>My first React</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
